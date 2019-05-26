df71632740d3c67d9db8abc2fe667867c78e205e
Asuka Takahashi <ask.ksaay.2510@gmail.com>
2019-05-26 03:06:44 +0000

    17の倍数である場合trueを返す処理追加

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 'use strict';
-
-module.exports = {
+/**
+ * 17の倍数である場合trueを返す。
+ */
+const seventeen = new Map();
+function isMultipleOfSeventeen(num){
+    if ( num % 17 === 0){
+        return seventeen.set(seventeen, true);
+    }
 }
+module.exports = {
+    isMultipleOfSeventeen : isMultipleOfSeventeen
+}
+
+
+// 配列に含まれる整数が 17 で割り切れるものだけにする seventeen モジュールを実装しています。
+// そのために、整数が 17 で割り切れるかどうかを判定する 
+// isMultipleOfSeventeen 関数を seventeen モジュールに実装してください。
